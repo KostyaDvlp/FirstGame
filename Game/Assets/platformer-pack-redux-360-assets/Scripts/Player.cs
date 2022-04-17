090945f4fbd24f00ef84852ab22c26fde303b824
KostyaDvlp <103059909+KostyaDvlp@users.noreply.github.com>
2022-04-17 17:43:07 +0300

    Add player, script player

---
+++ Game/Assets/platformer-pack-redux-360-assets/Scripts/Player.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float speed;
+    public float jumpHeight;
+    public Transform groundCheck;
+    bool isGrounded;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Flip();
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rb.velocity.y);
+    }
+
+    private void Update()
+    {
+        CheckGround();
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(transform.up * jumpHeight, ForceMode2D.Impulse);
+        }
+    }
+    void Flip()
+    {
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+        }
+        else
+        {
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+        }
+    }
+
+    void CheckGround()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, 0.2f);
+        isGrounded = colliders.Length > 1;
+    }
+}
